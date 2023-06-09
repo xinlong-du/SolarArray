8deb63fe926cb3615c41073618edfc8f8cb74d92
Xinlong Du <du.xinl@husky.neu.edu>
2023-06-09 16:37:32 -0700

    Verify equilibrium for dynamic analysis
    The small error is due to the inertia force of the lumped mass at element ends

--- FEA/Data/testEquilibrium.py
+++ FEA/Data/testEquilibrium.py
@@ -38,17 +38,17 @@ Jx_pu = 0.000954*in2m**4;  #torsional moment of inertia of section
 mass_pu = A_pu*rho_s;     #mass per unit length
 
 # define NODES-----------------------------------------------------------------
-node(1,             0.0, 0.0,          0.0)
-node(2,             0.0, 0.0,       7*in2m)
-node(3,             0.0, 0.0,      11*in2m)
-node(4,             0.0, 0.0,   92.25*in2m)
+node(1,             0.0, 0.0,    0.0)
+node(2,         30*in2m, 0.0,    0.0)
+node(3,         60*in2m, 0.0,    0.0)
+node(4,         90*in2m, 0.0,    0.0)
 
 # define BOUNDARY CONDITIONS---------------------------------------------------
 fix(1, 1, 1, 1, 1, 1, 1);
 
 # define ELEMENTS--------------------------------------------------------------
 postTransfTag = 1;
-vecxz = [1.0, 0.0, 0.0];
+vecxz = [0.0, 0.0, 1.0];
 geomTransf('Linear', postTransfTag, *vecxz);
 
 element('elasticBeamColumn', 101, *[1, 2], A_pu, Es, Gs, Jx_pu, Iy_pu, Iz_pu, postTransfTag, '-mass', mass_pu);
@@ -70,24 +70,49 @@ vfo.plot_modeshape(modenumber=1, scale=1); #plot mode shape 1
 vfo.plot_modeshape(modenumber=2, scale=1); #plot mode shape 2
 vfo.plot_modeshape(modenumber=3, scale=1); #plot mode shape 3
 
-# define loads-----------------------------------------------------------------
-F = 10.0; 
-timeSeries('Linear',1);
-pattern('Plain', 1, 1);
-load(4, *[0.0,  F, 0.0, 0.0, 0.0, 0.0]);
+#%% load wind tunnel test DATA
+import h5py
+filename = "../../../../RWDI/Wind Tunnel Data/tilt_n30deg.hdf5"
+
+with h5py.File(filename, "r") as f:
+    # get the key of interest; may or may NOT be a group
+    a_group_key = list(f.keys())[0]
+    # get the object names in the group and returns as a list
+    objNames = list(f[a_group_key])
+    # preferred methods to get dataset values
+    Cp = f[a_group_key]['Row1'][()]  # returns as a numpy array
+    dtNorm = f[a_group_key]['dtNorm'][()]
+
+#%% define LOADS---------------------------------------------------------------
+L=169.25*in2m;
+U=27.0;
+dt=0.02;
+rho_air=1.226;
+p=0.5*rho_air*U*U*Cp;
+x=L*np.array([0.125/2+0.25/2,0.25,0.25,0.25/2+0.125/2]);
+y=L*np.array([0.125/2+0.5/2,0.5/2+0.75/2,0.75/2+1/2,1/2+1.25/2,1.25/2+1/2,1/2+0.75/2,0.75/2+0.125/2]);
+A_trib=np.outer(x, y);
+A_trib=np.reshape(A_trib,(1,28),order='F');
+#A_trib=np.repeat(A_trib,np.shape(p)[0],axis=0);
+Force=np.multiply(p,A_trib);
+Force=Force.T.tolist()
+
+timeSeries('Path',1,'-dt',dt,'-values',*Force[1],'-prependZero');
+pattern('Plain',1,1);
+load(3, *[0.0, 1.0, 1.0, 0.0, 0.0, 0.0]);
+
+# define DAMPING
+rayleigh(0.0,0.0,0.0,2*0.02/(eigenValues[0]**0.5));
 
 # define ANALYSIS PARAMETERS---------------------------------------------------
 constraints('Plain');  # how it handles boundary conditions
-numberer('Plain');	   # renumber dof's to minimize band-width 
-system('BandGeneral'); # how to store and solve the system of equations in the analysis
+numberer('RCM');	   # renumber dof's to minimize band-width 
+system('UmfPack');     # how to store and solve the system of equations in the analysis
 test('NormDispIncr', 1.0e-08, 1000); # determine if convergence has been achieved at the end of an iteration step
-algorithm('Linear');
-integrator('LoadControl', 0.1)
-#Dincr = -0.01; #-0.00002
-                                  #Node,  dof, 1st incr, Jd,  min,   max
-#integrator('DisplacementControl', EndNode, 1,   Dincr,    1,  Dincr, -0.01);
-analysis('Static');	# define type of analysis static or transient
-analyze(10);
+algorithm('KrylovNewton');
+integrator('Newmark', 0.5, 0.25);
+analysis('Transient'); # define type of analysis static or transient
+ok = analyze(100, 0.02);
 print('Finished')
 
 #%% output forces on joints at the last time step, used for verification-------
@@ -96,4 +121,7 @@ eleForces101=eleForce(101);
 eleForces101Local=eleResponse(101, 'localForces')
 eleForces102=eleForce(102);
 eleForces102Local=eleResponse(102, 'localForces')
+eleForces103=eleForce(103);
+eleForces103Local=eleResponse(103, 'localForces')
 nodeForces2end=np.array(eleForces101Local[6:12])+np.array(eleForces102Local[0:6]);
+nodeForces3end=np.array(eleForces102Local[6:12])+np.array(eleForces103Local[0:6]);
