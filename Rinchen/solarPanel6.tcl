61ecbf4c30feeefe10072ee7055e6bc4fbc4a1d0
Xinlong Du <du.xinl@husky.neu.edu>
2023-12-24 23:47:21 -0800

    Tcl: change the direction of the bending moments

--- Rinchen/solarPanel6.tcl
+++ Rinchen/solarPanel6.tcl
@@ -225,8 +225,8 @@ loadConst -time 0.0; # maintains the load constant for the reminder of the analy
 
 # define RECORDERS
 #-------------------------------------------------------------
-recorder Node -file $dir/solarPanel1NyieldOffset.out -time -node $middleNode1 -dof 1 2 3 4 5 6 7 disp;
-recorder Node -file $dir/solarPanel2NyieldOffset.out -time -node $middleNode2 -dof 1 2 3 4 5 6 7 disp;
+recorder Node -file $dir/solarPanel1yieldOffsetTwNmoP.out -time -node $middleNode1 -dof 1 2 3 4 5 6 7 disp;
+recorder Node -file $dir/solarPanel2yieldOffsetTwNmoP.out -time -node $middleNode2 -dof 1 2 3 4 5 6 7 disp;
 
 # Define DISPLAY -------------------------------------------------------------
 DisplayModel3D DeformedShape;	 # options: DeformedShape NodeNumbers ModeShape
@@ -235,13 +235,13 @@ DisplayModel3D DeformedShape;	 # options: DeformedShape NodeNumbers ModeShape
 #------------------------------------------------------------- 
 pattern Plain 2 Linear {
   # NodeID, Fx, Fy, Fz, Mx, My, Mz, Bx
-  load $startNode1 0 0 0 0 0 [expr -4448.2216*25.4] 0; #the applied reference load is 1 kip-in
-  load $endNode1   0 0 0 0 0 [expr  4448.2216*25.4] 0;
-  load $startNode2 0 0 0 0 0 [expr -4448.2216*25.4] 0; #the applied reference load is 1 kip-in
-  load $endNode2   0 0 0 0 0 [expr  4448.2216*25.4] 0;
+  load $startNode1 0 0 0 0 0 [expr  4448.2216*25.4] 0; #the applied reference load is 1 kip-in
+  load $endNode1   0 0 0 0 0 [expr -4448.2216*25.4] 0;
+  load $startNode2 0 0 0 0 0 [expr  4448.2216*25.4] 0; #the applied reference load is 1 kip-in
+  load $endNode2   0 0 0 0 0 [expr -4448.2216*25.4] 0;
 }
 
-recorder plot $dir/solarPanel1N.out Displ-X 1200 10 300 300 -columns 5 1; # a window to plot the nodal displacements versus time
+recorder plot $dir/solarPanel1yieldOffsetTwNmoP.out Displ-X 1200 10 300 300 -columns 5 1; # a window to plot the nodal displacements versus time
 
 # define ANALYSIS PARAMETERS
 #------------------------------------------------------------------------------------
