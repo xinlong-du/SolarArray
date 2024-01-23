c27b781d64db043f26595837f9f721809a632243
Xinlong Du <du.xinl@husky.neu.edu>
2024-01-22 23:02:43 -0800

    TclRinchen for tests: plot up to the max test displacement

--- Rinchen/LateralBuckling/plotTests.m
+++ Rinchen/LateralBuckling/plotTests.m
@@ -4,27 +4,27 @@ purlinTwP=load('6CS2.5x065L262inFY.out');
 purlinTwN=load('6CS2.5x065L262inFYtwN.out');
 
 figure
-plot(purlinTwP(:,3)/25.4,purlinTwP(:,1),'k','LineWidth',1.0)
+plot(purlinTwP(1:160,3)/25.4,purlinTwP(1:160,1),'k','LineWidth',1.0)
 hold on
-plot(purlinTwN(:,3)/25.4,purlinTwN(:,1),'r--','LineWidth',1.0)
+plot(purlinTwN(1:160,3)/25.4,purlinTwN(1:160,1),'r--','LineWidth',1.0)
 legend('Positive initial twist','Negative initial twist')
 xlabel('Vertical disp. (in)')
 ylabel('Force on one purlin (kip)')
 grid on
 
 figure
-plot(purlinTwP(:,4)/25.4,purlinTwP(:,1),'k','LineWidth',1.0)
+plot(purlinTwP(1:160,4)/25.4,purlinTwP(1:160,1),'k','LineWidth',1.0)
 hold on
-plot(purlinTwN(:,4)/25.4,purlinTwN(:,1),'r--','LineWidth',1.0)
+plot(purlinTwN(1:160,4)/25.4,purlinTwN(1:160,1),'r--','LineWidth',1.0)
 legend('Positive initial twist','Negative initial twist')
 xlabel('Lateral disp. (in)')
 ylabel('Force on one purlin (kip)')
 grid on
 
 figure
-plot(purlinTwP(:,5),purlinTwP(:,1),'k','LineWidth',1.0)
+plot(purlinTwP(1:160,5),purlinTwP(1:160,1),'k','LineWidth',1.0)
 hold on
-plot(purlinTwN(:,5),purlinTwN(:,1),'r--','LineWidth',1.0)
+plot(purlinTwN(1:160,5),purlinTwN(1:160,1),'r--','LineWidth',1.0)
 legend('Positive initial twist','Negative initial twist')
 xlabel('Twist at midspan (rad)')
 ylabel('Force on one purlin (kip)')
