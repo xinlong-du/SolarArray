9534f8f0612628ec493803b75f2ae218dcaa369d
Xinlong Du <du.xinl@husky.neu.edu>
2024-01-27 11:57:15 -0800

    Py22P: plot duty cycles for 4 actuators

--- FEA/Data/dutyCyclesTX.m
+++ FEA/Data/dutyCyclesTX.m
@@ -18,18 +18,13 @@ for i=0:30:330
     end
 end
 
-%%
-[c,hist,edges,rmm,idx] = rainflow(ndLocYsAll{1}*39.37,fs); %convert m to inch
-
-histogram('BinEdges',edges','BinCounts',51*sum(hist,2))
-xlabel('Displacement range (in.)','FontSize',8,'FontName','Times New Roman')
-ylabel('Cycle counts','FontSize',8,'FontName','Times New Roman')
-set(gca,'YScale','log')
-set(gca,'FontSize',8,'FontName','Times New Roman')
-
 %% disp at node 1714
 fs=1/0.02;
 [c,hist,edges,rmm,idx] = rainflow(ndLocYsAll{1}*39.37,fs); %convert m to inch
+figure
+histogram('BinEdges',edges','BinCounts',51*sum(hist,2))
+set(gca,'YScale','log')
+
 edges2=0:0.2:2.2;
 bins=51*sum(hist,2);
 bins2=zeros(11,1);
@@ -44,6 +39,7 @@ xlabel('Displacement range (in.)','FontSize',8,'FontName','Times New Roman')
 ylabel('Cycle counts','FontSize',8,'FontName','Times New Roman')
 set(gca,'YScale','log')
 set(gca,'FontSize',8,'FontName','Times New Roman')
+xticks(0:0.2:2.2)
 % save figure
 figWidth=6;
 figHeight=3;
@@ -52,6 +48,97 @@ set(hfig,'PaperPosition',[0 0 figWidth figHeight]);
 fileout='.\figures\nd1714Disp.';
 print(hfig,[fileout,'tif'],'-r200','-dtiff');
 
+%% disp at node 1717
+fs=1/0.02;
+[c,hist,edges,rmm,idx] = rainflow(ndLocYsAll{2}*39.37,fs); %convert m to inch
+figure
+histogram('BinEdges',edges','BinCounts',51*sum(hist,2))
+set(gca,'YScale','log')
+
+edges2=0:0.2:2.4;
+bins=51*sum(hist,2);
+bins2=zeros(12,1);
+for i=0:10
+    bins2(i+1)=sum(bins(i*67+1:i*67+67));
+end
+bins2(11+1)=sum(bins(11*67+1:end));
+
+hfig=figure;
+histogram('BinEdges',edges2,'BinCounts',bins2)
+xlabel('Displacement range (in.)','FontSize',8,'FontName','Times New Roman')
+ylabel('Cycle counts','FontSize',8,'FontName','Times New Roman')
+set(gca,'YScale','log')
+set(gca,'FontSize',8,'FontName','Times New Roman')
+xticks(0:0.2:2.4)
+% save figure
+figWidth=6;
+figHeight=3;
+set(hfig,'PaperUnits','inches');
+set(hfig,'PaperPosition',[0 0 figWidth figHeight]);
+fileout='.\figures\nd1717Disp.';
+print(hfig,[fileout,'tif'],'-r200','-dtiff');
+
+%% disp at node 1814
+fs=1/0.02;
+[c,hist,edges,rmm,idx] = rainflow(ndLocYsAll{3}*39.37,fs); %convert m to inch
+figure
+histogram('BinEdges',edges','BinCounts',51*sum(hist,2))
+set(gca,'YScale','log')
+
+edges2=0:0.2:1.6;
+bins=51*sum(hist,2);
+bins2=zeros(8,1);
+for i=0:6
+    bins2(i+1)=sum(bins(i*100+1:i*100+100));
+end
+bins2(7+1)=sum(bins(7*100+1:end));
+
+hfig=figure;
+histogram('BinEdges',edges2,'BinCounts',bins2)
+xlabel('Displacement range (in.)','FontSize',8,'FontName','Times New Roman')
+ylabel('Cycle counts','FontSize',8,'FontName','Times New Roman')
+set(gca,'YScale','log')
+set(gca,'FontSize',8,'FontName','Times New Roman')
+xticks(0:0.2:2.4)
+% save figure
+figWidth=6;
+figHeight=3;
+set(hfig,'PaperUnits','inches');
+set(hfig,'PaperPosition',[0 0 figWidth figHeight]);
+fileout='.\figures\nd1814Disp.';
+print(hfig,[fileout,'tif'],'-r200','-dtiff');
+
+%% disp at node 1817
+fs=1/0.02;
+[c,hist,edges,rmm,idx] = rainflow(ndLocYsAll{4}*39.37,fs); %convert m to inch
+figure
+histogram('BinEdges',edges','BinCounts',51*sum(hist,2))
+set(gca,'YScale','log')
+
+edges2=0:0.2:1.8;
+bins=51*sum(hist,2);
+bins2=zeros(9,1);
+for i=0:7
+    bins2(i+1)=sum(bins(i*100+1:i*100+100));
+end
+bins2(8+1)=sum(bins(8*100+1:end));
+
+hfig=figure;
+histogram('BinEdges',edges2,'BinCounts',bins2)
+xlabel('Displacement range (in.)','FontSize',8,'FontName','Times New Roman')
+ylabel('Cycle counts','FontSize',8,'FontName','Times New Roman')
+set(gca,'YScale','log')
+set(gca,'FontSize',8,'FontName','Times New Roman')
+xticks(0:0.2:2.4)
+% save figure
+figWidth=6;
+figHeight=3;
+set(hfig,'PaperUnits','inches');
+set(hfig,'PaperPosition',[0 0 figWidth figHeight]);
+fileout='.\figures\nd1817Disp.';
+print(hfig,[fileout,'tif'],'-r200','-dtiff');
+
+%%
 function [ndLocYs]=forceDispResp(nodeDisp)
 nodeRec=[1701:1733,1801:1833,1301:1333,1401:1433,1501:1533]';
 
